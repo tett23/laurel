40240b383a79dbd61e9a872c040db23e4abc4923
tett23 <tett23@gmail.com>
2014-05-11 00:57:36 +0900

    ファイルを追加できるようにした

--- lib/laurel.rb
+++ lib/laurel.rb
@@ -4,4 +4,12 @@ module Laurel
   def self.create_project(name)
     Dir.mkdir(name)
   end
+
+  def self.add_page(name)
+    FileUtils.touch(name)
+  end
+
+  def self.generate_page_name
+    Digest::MD5.hexdigest(Time.now.to_s)+'.textile'
+  end
 end
