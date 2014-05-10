a02019fd040491cc68b9c0f4e26e97e49a0052fb
tett23 <tett23@gmail.com>
2014-05-11 05:11:01 +0900

    ファイルの追加

--- lib/laurel.rb
+++ lib/laurel.rb
@@ -11,14 +11,44 @@ module Laurel
     FileUtils.cp_r(template_dir, name) unless Dir.exists?(name)
   end
 
-  def self.add_page(name)
-    dir = nil
-    dir = name.gsub(/^.+\//, '') if name =~ /\//
+  def self.add_page(name=nil)
+    path = self.generate_page_name(name)
+    path += '.textile' unless File.extname(path) == '.textile'
+    dir = path.gsub(/^(.+)\/(.+)/, '\1')
+    FileUtils.mkdir_p(dir)
+    FileUtils.touch(path)
 
-    FileUtils.touch(name)
+    path
   end
 
-  def self.generate_page_name
-    Digest::MD5.hexdigest(Time.now.to_s)+'.textile'
+  private
+  def self.generate_page_name(flagment=nil)
+    filename = if flagment.nil?
+      file = Digest::MD5.hexdigest(Time.now.to_s)+'.textile'
+
+      File.expand_path(File.join(Laurel::Config.directories.posts, file))
+    elsif self.file?(flagment)
+      File.expand_path(flagment)
+    elsif self.directory?(flagment)
+      file = Digest::MD5.hexdigest(Time.now.to_s)+'.textile'
+
+      File.expand_path(File.join(flagment, file))
+    end
+
+    filename
+  end
+
+  def self.file?(flagment)
+    return false if flagment.include?('/')
+
+    expanded_flagment = File.expand_path(flagment)
+    not File.directory?(expanded_flagment)
+  end
+
+  def self.directory?(flagment)
+    return true if flagment.include?('/')
+
+    expanded_flagment = File.expand_path(flagment)
+    File.directory?(expanded_flagment)
   end
 end
--- spec/lib/laurel_spec.rb
+++ spec/lib/laurel_spec.rb
@@ -1,4 +1,33 @@
 require 'spec_helper'
 
 describe Laurel do
+  describe :add_page do
+    it '引数なし' do
+      path = Laurel.add_page()
+      expect(File).to exist(path)
+    end
+
+    context '引数あり' do
+      it '新規ファイル' do
+        path = Laurel.add_page('foo.textile')
+        expect(File).to exist(path)
+      end
+
+      it 'ディレクトリを掘る必要がある' do
+        path = Laurel.add_page('foo/bar.textile')
+        expect(File).to exist(path)
+      end
+
+      it '拡張子を追加させる' do
+        path = Laurel.add_page('hoge')
+        expect(File.extname(path)).to eq('.textile')
+        expect(File).to exist(path)
+      end
+
+      it 'ディレクトリを指定' do
+        path = Laurel.add_page('config')
+        expect(File).to exist(path)
+      end
+    end
+  end
 end
